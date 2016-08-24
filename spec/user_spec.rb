e7edf0b9a9721b9c62e16218fb0d2b8dee918ac8
timbertinker <forest310125@gmail.com>
2016-08-24 11:21:53 +0200

    Made the first test work

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,11 +1,7 @@
 require './lib/models/model'
 
 describe User do
-  #subject { described_class.new(password: '123') }
-
-# before do
-#   test = subject.authenticate("123")
-# end
+  
   before do
     @user = User.create(username: "user")
     @user.password = "123"
@@ -13,6 +9,6 @@ describe User do
   end
 
   it 'returns true when the correct password is entered' do
-    expect(@user.password).to eq "123"
+    expect(@user.authenticate("123")).to eq true
   end
 end
