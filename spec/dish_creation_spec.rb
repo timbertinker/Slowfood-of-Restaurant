fd8218a15628d37659fe7dc39aecae22e5d72adc
timbertinker <forest310125@gmail.com>
2016-08-26 16:27:17 +0200

    set the dish class #13

---
+++ spec/dish_creation_spec.rb
@@ -0,0 +1,21 @@
+describe Dish do
+
+  before do
+    @dish = Dish.new(name:"salad", price:70, category: "main")
+    @dish.save
+  end
+
+it 'should have a name' do
+  #expect
+end
+
+it 'should have a price' do
+
+end
+
+it 'should have a category' do
+
+end
+
+
+end
